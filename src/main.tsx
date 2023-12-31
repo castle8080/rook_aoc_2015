8cbb081178304152f949ee7ea20c87f8a80bc236
Bryan Castillo <castillo.bryan@gmail.com>
2023-12-30 20:52:08 -0800

    Doing day3.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {
@@ -11,11 +10,13 @@ import './index.css'
 import App from './App.tsx'
 import Problem1 from './problems/problem1.tsx'
 import Problem2 from './problems/problem2.tsx'
+import Problem3 from './problems/problem3.tsx'
 
 const router = createBrowserRouter([
     { path: "/", element: <App/>, },
     { path: "/problem1", element: <Problem1/>, },
     { path: "/problem2", element: <Problem2/>, },
+    { path: "/problem3", element: <Problem3/>, },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ src/problems/problem3.tsx
@@ -0,0 +1,66 @@
+import _ from 'lodash'
+import Problem from '../problem'
+
+function parse_input(input: string): Array<[number, number]> {
+    const content = input.trim();
+    return _.map(content, c => { switch (c) {
+        case "<": return [-1, 0];
+        case ">": return [1, 0];
+        case "^": return [0, 1];
+        case "v": return [0, -1];
+        default: {
+            throw new Error(`Invalid input: ${c}`);
+        }
+    }});
+}
+
+async function part1(input: string): Promise<number> {
+    const movements = parse_input(input);
+    const houses = new Map<string, number>();
+
+    let cur: [number, number] = [0, 0];
+    houses.set(cur.toString(), 1);
+    for (const [dx, dy] of movements) {
+        cur = [cur[0] + dx, cur[1] + dy];
+        const k = cur.toString();
+        houses.set(k, (houses.get(k) || 0) + 1);
+    }
+
+    return houses.size;
+}
+
+async function part2(input: string): Promise<number> {
+    const movements = parse_input(input);
+    const houses = new Map<string, number>();
+
+    let santa: [number, number] = [0, 0];
+    let robo_santa: [number, number] = [0, 0];
+    let santas = [santa, robo_santa];
+
+    houses.set(santa.toString(), 1);
+
+    for (const [idx, [dx, dy]] of movements.entries()) {
+        const santa_id = idx % santas.length;
+        santas[santa_id][0] += dx;
+        santas[santa_id][1] += dy;
+
+        const k = santas[santa_id].toString();
+        houses.set(k, (houses.get(k) || 0) + 1);
+    }
+
+    return houses.size;
+}
+
+function Problem3() {
+    return (
+        <Problem
+            day = { 3 }
+            part1 = { part1 }
+            part2 = { part2 }
+            inputs = {[ "input/input_03.txt", "input/input_03_test.txt" ]}
+            problem_link = "https://adventofcode.com/2015/day/3"
+        />
+    )
+}
+
+export default Problem3
