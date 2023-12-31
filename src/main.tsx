6a96c07fd1309284b364b3ea0a9b4db885494037
Bryan Castillo <castillo.bryan@gmail.com>
2023-12-31 09:52:48 -0800

    Doing day 4. Its all brute forced though.

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,14 @@ import App from './App.tsx'
 import Problem1 from './problems/problem1.tsx'
 import Problem2 from './problems/problem2.tsx'
 import Problem3 from './problems/problem3.tsx'
+import Problem4 from './problems/problem4.tsx'
 
 const router = createBrowserRouter([
     { path: "/", element: <App/>, },
     { path: "/problem1", element: <Problem1/>, },
     { path: "/problem2", element: <Problem2/>, },
     { path: "/problem3", element: <Problem3/>, },
+    { path: "/problem4", element: <Problem4/>, },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ src/problems/problem4.tsx
@@ -0,0 +1,61 @@
+import _, { first, last } from 'lodash'
+import Problem from '../problem'
+import md5 from 'md5'
+
+function count_leading_zeros(s: string): number {
+    let i = 0;
+    for (; i < s.length; i++) {
+        if (s[i] != '0') {
+            return i;
+        }
+    }
+    return 0;
+}
+
+function find_md5_leading_zeros(input: string, needed_lz: number, starting_num: number) {
+    let n = starting_num;
+    let last_leading = 0;
+    let first_leading = [];
+
+    while (true) {
+        const md5_str = md5(input + n);
+        const lz = count_leading_zeros(md5_str);
+        if (lz > last_leading) {
+            first_leading.push({ leading_zeros: lz, n: n });
+            last_leading = lz;
+            if (lz == needed_lz) {
+                return first_leading;
+            }
+        }
+        n += 1;
+    }
+}
+
+async function part1(input: string): Promise<number> {
+    input = input.trim();
+    const result = find_md5_leading_zeros(input, 5, 0);
+    console.log(result);
+    return result[result.length - 1].n;
+}
+
+async function part2(input: string): Promise<number> {
+    // Is there a way around brute forcing this?
+    input = input.trim();
+    const result = find_md5_leading_zeros(input, 6, 0);
+    console.log(result);
+    return result[result.length - 1].n;
+}
+
+function Problem4() {
+    return (
+        <Problem
+            day = { 4 }
+            part1 = { part1 }
+            part2 = { part2 }
+            inputs = {[ "input/input_04.txt", "input/input_04_test.txt" ]}
+            problem_link = "https://adventofcode.com/2015/day/4"
+        />
+    )
+}
+
+export default Problem4;
